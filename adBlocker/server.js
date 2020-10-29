04e1c4895d8e5a457cc0696b23bf2b4dfa8a5012
Aniket <aniketindian8@gmail.com>
2020-10-29 12:13:47 +0530

    added adBlocker

---
+++ adBlocker/server.js
@@ -0,0 +1,27 @@
+chrome.webRequest.onBeforeRequest.addListner(
+    function(detils) {return {cancel:true}},
+    {urls : blockers},
+    ["blocking"]
+)
+
+blockers = [
+    "*://*.doubkeclick.net/*",
+    "*://*.zedo.com/*",
+    "*://*.scorecardresearch.com/*",
+    "*://*.quantserve.com/*",
+    "*://*.exponential.com/*",
+    "*://*.adbrite.com/*",
+    "*://*.google-analytics.com/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.partner.googleservices.com/*",
+    "*://*.adzerk.com/*",
+    "*.//*.openx.com/*",
+    "*.//*.broadstreetads.com/*",
+    "*.//*.adcolt.com/*",
+    "*.//*.adbutler.com/*",
+    "*.//*.epom.com/*",
+    "*.//*.revive-adserver.com/*",
+    "*.//*.oiopublisher.com/*",
+
+]
