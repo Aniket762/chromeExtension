f61af337098c62f6c304bc3d8f0aa3afdad1f669
Sumana Basu <sumana.basu2001@gmail.com>
2020-11-02 00:06:00 +0530

    Minor URL error fixed

--- adBlocker/server.js
+++ adBlocker/server.js
@@ -10,13 +10,13 @@ const blockers = [
     "*://*.googlesyndication.com/*",
     "*://*.partner.googleservices.com/*",
     "*://*.adzerk.com/*",
-    "*.//*.openx.com/*",
-    "*.//*.broadstreetads.com/*",
-    "*.//*.adcolt.com/*",
-    "*.//*.adbutler.com/*",
-    "*.//*.epom.com/*",
-    "*.//*.revive-adserver.com/*",
-    "*.//*.oiopublisher.com/*",
+    "*://*.openx.com/*",
+    "*://*.broadstreetads.com/*",
+    "*://*.adcolt.com/*",
+    "*://*.adbutler.com/*",
+    "*://*.epom.com/*",
+    "*://*.revive-adserver.com/*",
+    "*://*.oiopublisher.com/*",
 ]
 // you can add the list of ad servers from which you don't ads
 
@@ -25,6 +25,4 @@ chrome.webRequest.onBeforeRequest.addListener(
     {urls : blockers},
     ["blocking"]
 )
-
-
 
